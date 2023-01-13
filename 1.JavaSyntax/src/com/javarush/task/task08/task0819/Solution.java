46a3e449dafa34763ecb470e8c7f4e5776d92577
greenolls <a.kuleshov@hypeauditor.com>
2023-01-13 19:16:14 +0300

    jr

---
+++ 1.JavaSyntax/src/com/javarush/task/task08/task0819/Solution.java
@@ -0,0 +1,38 @@
+package com.javarush.task.task08.task0819;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/* 
+Set из котов
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        Set<Cat> cats = createCats();
+
+        //напишите тут ваш код. step 3 - пункт 3
+        for (Cat cat : cats) {
+            cats.remove(cat);
+            break;
+        }
+
+        printCats(cats);
+    }
+
+    public static Set<Cat> createCats() {
+        Set<Cat> cats = new HashSet<>();
+        for (int i=0; i<3; i++) {
+            cats.add(new Cat());
+        }
+        return cats;
+    }
+
+    public static void printCats(Set<Cat> cats) {
+        // step 4 - пункт 4
+        cats.forEach(System.out::println);
+    }
+
+    // step 1 - пункт 1
+    public static class Cat {}
+}
